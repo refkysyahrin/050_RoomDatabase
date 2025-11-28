47adb78d847353e04de4eb5bd62ccff0526d4df9
refkysyahrin <refkymuhammadsyahrin@gmail.com>
2025-11-28 18:31:03 +0700

    membuat objek DestinasiEntry untuk routing halaman tambah siswa

---
+++ app/src/main/java/com/example/pam_activity7_roomdatabase/view/route/DestinasiEntry.kt
@@ -0,0 +1,8 @@
+package com.example.pam_activity7_roomdatabase.view.route
+
+import com.example.pam_activity7_roomdatabase.R
+
+object DestinasiEntry: DestinasiNavigasi {
+    override  val  route = "item_entry"
+    override val titleRes = R.string.entry_siswa
+}
