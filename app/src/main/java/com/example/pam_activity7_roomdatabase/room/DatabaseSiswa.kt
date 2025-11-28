4537d4bd5dca6762ac7f4b47a229ba7be7ae9b1e
refkysyahrin <refkymuhammadsyahrin@gmail.com>
2025-11-28 15:33:27 +0700

    membuat class DatabaseSiswa dan konfigurasi instance singleton

--- app/src/main/java/com/example/pam_activity7_roomdatabase/room/DatabaseSiswa.kt
+++ app/src/main/java/com/example/pam_activity7_roomdatabase/room/DatabaseSiswa.kt
@@ -1,3 +1,26 @@
 package com.example.pam_activity7_roomdatabase.room
 
-data class DatabaseSiswa()
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(entities = [Siswa::class], version = 1, exportSchema = false)
+abstract class DatabaseSiswa : RoomDatabase(){
+    abstract fun siswaDao() : SiswaDao
+
+    companion object {
+        @Volatile
+        private var Instance: DatabaseSiswa? = null
+
+        fun getDatabase(context: Context): DatabaseSiswa {
+            return (Instance?: synchronized(lock = this){
+                Room.databaseBuilder(
+                    context, klass = DatabaseSiswa::class.java,
+                    name = "siswa_database")
+                        .build().also { Instance=it}
+            }
+                    )
+        }
+    }
+}
