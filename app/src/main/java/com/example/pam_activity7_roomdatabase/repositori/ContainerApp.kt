42a382182fb592e5459f7955561593ed430a7902
refkysyahrin <refkymuhammadsyahrin@gmail.com>
2025-11-28 15:51:25 +0700

    implementasi manual dependency injection dan konfigurasi kelas Application

---
+++ app/src/main/java/com/example/pam_activity7_roomdatabase/repositori/ContainerApp.kt
@@ -0,0 +1,27 @@
+package com.example.pam_activity7_roomdatabase.repositori
+
+import android.app.Application
+import android.content.Context
+import com.example.pam_activity7_roomdatabase.room.DatabaseSiswa
+
+interface  ContainerApp {
+    val repositoriSiswa : RepositoriSiswa
+}
+
+class  ContainerDataApp(private val context: Context) :
+    ContainerApp {
+    override val repositoriSiswa: RepositoriSiswa by lazy {
+        OfflineRepositoriSiswa(
+            siswaDao = DatabaseSiswa.getDatabase(context).siswaDao())
+    }
+}
+
+class AplikasiSiswa : Application() {
+
+    lateinit var container: ContainerApp
+
+    override fun onCreate() {
+        super.onCreate()
+        container = ContainerDataApp(context = this)
+    }
+}
