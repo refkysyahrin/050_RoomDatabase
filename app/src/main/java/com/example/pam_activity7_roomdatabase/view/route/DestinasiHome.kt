15ee5ef179915d2ca9050bd572882ff3a721830c
refkysyahrin <refkymuhammadsyahrin@gmail.com>
2025-11-28 18:31:33 +0700

    membuat objek DestinasiHome untuk routing halaman utama

---
+++ app/src/main/java/com/example/pam_activity7_roomdatabase/view/route/DestinasiHome.kt
@@ -0,0 +1,8 @@
+package com.example.pam_activity7_roomdatabase.view.route
+
+import com.example.pam_activity7_roomdatabase.R
+
+object DestinasiHome : DestinasiNavigasi {
+    override val route = "home"
+    override val titleRes = R.string.app_name
+}
